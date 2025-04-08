e7898304bcd81b201f374e4df32c12dbba233553
ClementCar <clementcarcenac@gmail.com>
2025-04-08 17:12:22 +0200

    analytics

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+import { inject } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights';
+
+inject();
+injectSpeedInsights();
+
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
